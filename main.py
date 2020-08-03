f9e74ae77160de720a8bcf73e3d0fa6e2827318a
Miley-CS <mileyq88@gmail.com>
2020-08-02 21:38:03 -0700

    added class in main.py

---
+++ main.py
@@ -0,0 +1,7 @@
+class Person:
+	def __init__(self, name, personality, apperence, strength):
+		type_person = 'human'
+		self.name = name
+		self.personality = personality
+		self.apperance = apperance
+		self.strength = strength
